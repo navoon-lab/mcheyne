10dab4989cc6b37a5c5df7a471a349c725a78244
navoon-lab <navoon@gmail.com>
2026-01-01 17:51:31 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("mcheyne").then(cache => {
-      return cache.addAll(["index.html", "mcheyne-plan.json"]);
-    })
-  );
-});
